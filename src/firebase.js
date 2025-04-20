1cdee2a09b2f66ea7f65a0e152cc80e6e59520cf
Udipta Basumatari <udipta@Udiptas-MacBook-Air.local>
2025-04-20 22:07:13 +0100

    chore: use environment variables for Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_PIDV8pkLv4kl3nnU5zgYe6gy_EOkki8",
-  authDomain: "travel-tracker-web.firebaseapp.com",
-  projectId: "travel-tracker-web",
-  storageBucket: "travel-tracker-web.firebasestorage.app",
-  messagingSenderId: "971416350629",
-  appId: "1:971416350629:web:a46522541840eb4e88024c",
-  measurementId: "G-WQPEFV7LT2",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
